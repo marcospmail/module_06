9ec3b0fcbd8691f0ac4ebac0272ff7556e150e1c
Marcos Paulo <marcosp.mail@gmail.com>
2019-10-26 00:29:47 -0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+
+    'react/state-in-constructor': 'never',
+    'react/static-property-placement': 'warn',
+    'react/jsx-props-no-spreading': [{
+      'html': 'ignore' | 'enforce',
+      'custom': 'ignore' | 'enforce',
+      'explicitSpread': 'ignore' | 'enforce'
+    }],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js']
+      }
+    ],
+    'import/prefer-default-export': 'off'
+  },
+};
---
+++ src/pages/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container >
+    </Container>
+  );
+}
+
+Main.navigationOptions = {
+  title: 'Usuários'
+}
